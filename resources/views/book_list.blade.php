ded55a692d7a30f1ce57b8122799bd2f10c873a3
上村一平 <ippeikamimura@uemuraippeinoMacBook-Pro.local>
2017-12-26 18:11:31 +0900

    初コミット

---
+++ resources/views/book_list.blade.php
@@ -0,0 +1,82 @@
+@extends('layouts.common')
+
+@section('content')
+
+<div class="panel-body">
+  <!-- バリテーションエラーの表示に使用 -->
+  @include('common.errors')
+  <!-- バリテーションエラーの表示に使用 -->
+
+  <!-- 本登録フォーム -->
+  <form action="{{url('book_list')}}" method="post">
+    {{csrf_field()}}
+    <table border="1">
+      <tr>
+        <td>商品名</td>
+        <td><input type="text" name="pro_name"></td>
+      </tr>
+      <tr>
+        <td>商品名（英語表記）</td>
+        <td><input type="text" name="pro_name_en"></td>
+      </tr>
+      <tr>
+        <td>価格（円）</td>
+        <td><input type="text" name="pro_price"></td>
+      </tr>
+      <tr>
+        <td>画像</td>
+        <td><input type="text" name="pro_thumbnail"></td>
+      </tr>
+      <tr>
+        <td>ジャンル</td>
+        <td><input type="text" name="pro_genre"></td>
+      </tr>
+      <tr>
+        <td>著者</td>
+        <td><input type="text" name="pro_author"></td>
+      </tr>
+      <tr>
+        <td>原作者</td>
+        <td><input type="text" name="pro_original_author"></td>
+      </tr>
+      <tr>
+        <td>発売日</td>
+        <td><input type="text" name="pro_release_date"></td>
+      </tr>
+      <tr>
+        <td>出版社</td>
+        <td><input type="text" name="pro_publisher"></td>
+      </tr>
+      <tr>
+        <td>レーベル</td>
+        <td><input type="text" name="pro_label"></td>
+      </tr>
+      <tr>
+        <td>作品詳細</td>
+        <td><textarea name="pro_description" id="" cols="30" rows="10"></textarea></td>
+      </tr>
+      <tr>
+        <td>書籍サイズ</td>
+        <td><input type="text" name="pro_size"></td>
+      </tr>
+      <tr>
+        <td>書籍重量</td>
+        <td><input type="text" name="pro_weight"></td>
+      </tr>
+      <tr>
+        <td>在庫数</td>
+        <td><input type="text" name="pro_stock"></td>
+      </tr>
+      <tr>
+        <td>ISBN_NO</td>
+        <td><input type="text" name="pro_isbn"></td>
+      </tr>
+    </table>
+    <!-- 本登録ボタン -->
+    <button type="submit" class="btn-default">
+      <i class="glyphicon glyphicon-plus"></i>SAVE
+    </button>
+  </form>
+</div>
+
+@endsection
---
+++ resources/views/layouts/common.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Booquet</title>
+  <!-- CSSとJavaScript -->
+</head>
+<body>
+  <div class="container">
+    <nav class="navbar navbar-default">
+      <h1>NavBarだよ</h1>
+      <!-- ナビバーの表示領域 -->
+    </nav>
+  </div>
+
+  @yield('content')
+  
+</body>
+</html>
