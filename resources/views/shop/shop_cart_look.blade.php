d6326b9d5f20ac71734211a33047a923d33aa477
上村一平 <ippeikamimura@uemuraippeinoMacBook-Pro.local>
2017-12-31 18:06:43 +0900

    カート機能色々足りない。

--- resources/views/shop/shop_cart_look.blade.php
+++ resources/views/shop/shop_cart_look.blade.php
@@ -1,15 +1,24 @@
 @extends('layouts.shop_common')
 
-@section('content1')
-<table>
-@for($i=0; $i<count($cart); $i++)
-<tr>
-  <td>
-    <img src="{{asset('pro_img/'.$product[$i]->pro_thumbnail)}}" alt="" style="height: 150px;">
-  </td>
-</tr>
-  
-@endfor
-</table>
+@section('content3')
+<div class="container">
+  <table class="table">
+    @for($i=0; $i<count($cart); $i++)
+      <tr>
+        <td>
+          <img src="{{asset('pro_img/'.$product[$i]->pro_thumbnail)}}" alt="" style="height: 150px;">
+        </td>
+        <td>
+          ×{{$quantity[$i]}}
+        </td>
+        <td>
+          {{$product[$i]->pro_price.'円'}}
+        </td>
+      </tr>
+      
+    @endfor
+  </table>
+</div>
+
 
 @endsection
