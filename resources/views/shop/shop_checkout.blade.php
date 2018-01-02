720b63daa3b7dd01d98d5d35f536635ccf5c0392
上村一平 <ippeikamimura@uemuraippeinoMacBook-Pro.local>
2018-01-02 21:00:21 +0900

    、、、

--- resources/views/shop/shop_checkout.blade.php
+++ resources/views/shop/shop_checkout.blade.php
@@ -56,6 +56,71 @@
       </tr>
     </table>
     <h3>Payment Method</h3>
+    <table>
+      <tr>
+        <td>
+          Payment Type:
+        </td>
+        <td>
+          <input type="radio" name="c_pay_type" id="c_pay_type1" value="Credit" checked="checked"><label for="c_pay_type1">Credit</label>
+          <input type="radio" name="c_pay_type" id="c_pay_type2" value="Paypal"><label for="c_pay_type2">Paypal</label>
+          <input type="radio" name="c_pay_type" id="c_pay_type3" value="Cash"><label for="c_pay_type3">Cash on delivery</label>
+        </td>
+      </tr>
+      <tr>
+        <td>
+          Card Type:
+        </td>
+        <td>
+          <input type="radio" name="c_card_type" id="c_card_type1" value="Visa" checked="checked"><label for="c_card_type1">Visa</label>
+          <input type="radio" name="c_card_type" id="c_card_type2" value="MasterCard"><label for="c_card_type2">MasterCard</label>
+          <input type="radio" name="c_card_type" id="c_card_type3" value="Amex"><label for="c_card_type3">Amex</label>
+          <input type="radio" name="c_card_type" id="c_card_type4" value="JCB"><label for="c_card_type4">JCB</label>
+        </td>
+      </tr>
+      <tr>
+        <td>
+          Card Number:
+        </td>
+        <td>
+          <input type="text" name="c_card_number">
+        </td>
+      </tr>
+      <tr>
+        <td>
+          Expiration Date:
+        </td>
+        <td>
+          <select name="c_card_month" id="">
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+            <option value="10">10</option>
+            <option value="11">11</option>
+            <option value="12">12</option>
+          </select>
+          <select name="c_card_year" id="">
+            <option value="2018">2018</option>
+            <option value="2019">2019</option>
+            <option value="2020">2020</option>
+          </select>
+        </td>
+      </tr>
+      <tr>
+        <td>
+          Security Code:
+        </td>
+        <td>
+         <input type="text" name="c_card_security_code">
+        </td>
+      </tr>
+    </table>
     
     
     <button type="submit" class="btn btn-primary">Submit</button>
