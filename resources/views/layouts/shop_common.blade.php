dbcbbce2664a2e6873990f05bc670acd70bf6e6d
山下功介 <toxicsound.ksk@gmail.com>
2018-01-04 15:20:15 +0900

    検索結果ページ完成 (ページネーションは後でやります。。)

--- resources/views/layouts/shop_common.blade.php
+++ resources/views/layouts/shop_common.blade.php
@@ -13,10 +13,6 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
   <!-- self css -->
   <link rel="stylesheet" href="{{asset('css/common_shop.css')}}">
-  <!-- jquery -->
-  <!-- カルーセル用プラグインCSS -->
-  <link rel="stylesheet" type="text/css" href="{{asset('slick/slick.css')}}">
-  <link rel="stylesheet" type="text/css" href="{{asset('slick/slick-theme.css')}}">
 </head>
 <body>
 
@@ -113,8 +109,8 @@
 <!-- <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script> -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
-<!-- カルーセル用プラグイン -->
-<script type="text/javascript" src="{{asset('slick/slick.min.js')}}"></script>
+<!-- ページネーション用プラグイン -->
+<script type="text/javascript" src="{{asset('js/jquery.bootpag.js')}}"></script>
 <!-- self JS-->
 <script type="text/javascript" src="{{asset('js/shop_top.js')}}"></script>
 </body>
--- resources/views/shop/shop_result_page.blade.php
+++ resources/views/shop/shop_result_page.blade.php
@@ -1,48 +1,53 @@
 @extends('layouts.shop_common')
 
 @section('content1')
-
-
-
-<h1 class="question">Result::{{$question}}</h1>
-
-
-
-@endsection
-
-@section('content3')
-<div class="container result-wrap">
-  @if(count($products)>0)
-    @foreach($products as $product)
-    <div class="result-item">
-      <div>
-        <a href="{{url('shop_item_page/'.$product->id)}}">
-          <img src="./pro_img/{{$product->pro_thumbnail}}" alt="" style="height: 150px; display: block; margin: 0 auto;">
-        </a>
-      </div>
-      <div>
-        {{$product->pro_name}}
-      </div>
-      <div>
-        {{$product->pro_author}}
-      </div>
-      <div>
-      <?php
-         $number =$product->pro_price;
-        // 3桁ごとにカンマ区切りで出力
-        echo number_format($number);
-        ?>円
-      </div>
-      <div>
-        ★★★★★ 4.4
-      </div>
-      <div>
-        in Stock
-      </div>
+<div class="result_page_wrapper">
+    <div class="pro-item-area_left">
+        <div>
+            <p>Category</p>
+        </div>
+          <a href="">Tokyo</a>
+          <a href="">Kyoto</a>
+          <a href="">Gokudo</a>
+          <a href="">Temple</a>
+          <a href="">Art</a>
+          <a href="">Fashion</a>
+          <a href="">Music</a>
+          <a href="">Other</a>
     </div>
-    @endforeach
-  @endif
-
-</div>
+    
+    <div class="result_main_section">
+        <div class="result_main_title">
+          <span>Result ::</span><h1 class="question">{{$question}}</h1>
+        </div>
+        <div class="container result-wrap">
+              @if(count($products)>0)
+                @foreach($products as $product)
+                <div class="result-items">
+                  <a class="result-item" href="{{url('shop_item_page/'.$product->id)}}">
+                        <img src="./pro_img/{{$product->pro_thumbnail}}" alt="" style="height: 150px; display: block; margin: 0 auto;">
+                        <p>｢{{$product->pro_name}}｣</p>
+                        <p>{{$product->pro_author}}</p>
+                        <p>$
+                          <?php
+                        $number =$product->pro_price;
+                        // 3桁ごとにカンマ区切りで出力
+                        echo number_format($number);
+                        ?>
+                      </p>
+                      <p>★★★★★ 4.4</p>
+                      @if($product->pro_stock>0)
+                      <p><span class="fa fa-check-circle-o"></span>In Stock</p>
+                      @else
+                      <p><span class="fa fa-check-circle-o"></span>Out of Stock</p>
+                      @endif
+                    </a>
+                </div>
+                @endforeach
+              @endif
+        </div>
+    </div>
   
+    
+  </div>
 @endsection
